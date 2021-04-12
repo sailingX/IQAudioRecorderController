317dae9f4054e20a8204d99f72c7ca128393cb6e
sailingX <sailingsz8@126.com>
2021-04-12 16:39:07 +0800

    转换为库文件提供

---
+++ IQAudioRecorderControllerFrameWork/IQAudioRecorderControllerFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  IQAudioRecorderControllerFrameWork.h
+//  IQAudioRecorderControllerFrameWork
+//
+//  Created by Hawk Lei on 2021/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IQAudioRecorderControllerFrameWork.
+FOUNDATION_EXPORT double IQAudioRecorderControllerFrameWorkVersionNumber;
+
+//! Project version string for IQAudioRecorderControllerFrameWork.
+FOUNDATION_EXPORT const unsigned char IQAudioRecorderControllerFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IQAudioRecorderControllerFrameWork/PublicHeader.h>
+
+
